a95b75688b60f29c049673134fa0c94e9ea215f7
Josh Weinstein <weinstein.josh@gmail.com>
2013-06-11 01:55:25 -0400

    Keys are strings.

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require './is_question.rb'
 post '/detectors/question' do
   content_type :json
   params_json = JSON.parse(request.body.read)
-  question_text = params_json[:question_text]
+  question_text = params_json['question_text']
   result = IsQuestion.verify(question_text)
   {result: result}.to_json
 end
